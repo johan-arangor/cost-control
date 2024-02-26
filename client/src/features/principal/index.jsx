2673a9355db6ae53e6ffaa6fd5f6cf3553c2a262
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2024-02-26 07:54:29 -0500

    add new module revenues
    add new module revenues to function modals and api Rest

--- client/src/features/principal/index.jsx
+++ client/src/features/principal/index.jsx
@@ -1,11 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import imageIngreso from '../../assets/images/ingresos.png';
 import imageGasto from '../../assets/images/gastos.png';
 import '../../../src/assets/css/principal.css';
-
 import { Container, Card, Row, Col, Image } from 'react-bootstrap';
+import Revenues from './Revenues';
 
 export default function Principal() {
+    const [getModalRevenues, setModalRevenues] = useState(false);
+
+    const stateModalRevenue = () => {
+        setModalRevenues(!getModalRevenues);
+    }
+
     return(
         <Fragment>
             <Container>
@@ -18,8 +24,16 @@ export default function Principal() {
                     </Card.Title>
                     <Row className='text-center'>
                         <Col md={6} sm={12}>
-                            <Container className='buttonPpal ingreso'>
-                                <Image src={ imageIngreso } fluid width={300} height={300} border='0' />
+                            <Container 
+                                className='buttonPpal ingreso'
+                                onClick={() => setModalRevenues(true)}
+                            >
+                                <Row>
+                                    <h2>Registros</h2>
+                                </Row>
+                                {/* <Row>
+                                    <Image src={ imageIngreso } fluid width={300} height={300} border='0' title='Registros' />
+                                </Row> */}
                             </Container>
                         </Col>
                         <Col md={6} sm={12}>
@@ -30,6 +44,12 @@ export default function Principal() {
                     </Row>
                 </Card>
             </Container>
+            {getModalRevenues && (
+                <Revenues
+                    stateModalRevenue={stateModalRevenue}
+                    getStateModal={getModalRevenues}
+                />
+            )}
         </Fragment>
     );
 }
